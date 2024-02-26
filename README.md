883202b7486187c3a3a1ad8c1e0c17f511a67a9a
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-26 20:10:35 +0600

    Deploy live link

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+live link : https://nimble-tanuki-a7c6e7.netlify.app
 
+Back-End Link : https://github.com/sajjadujjamanemon09/Redux-server
+
+Front-End Link : https://github.com/sajjadujjamanemon09/Redux
--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://redux-server-fawn.vercel.app" }),
 
   endpoints: () => ({}),
 });
